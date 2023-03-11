931f200936c19b94a43dce7d54bf6105f9d67aa5
Oleksii Dumenko <oleksii.dumenko@gmail.com>
2023-03-11 12:06:35 +0100

    added foreign key and sessions

--- rest/app/__init__.py
+++ rest/app/__init__.py
@@ -5,11 +5,21 @@ import hashlib
 
 
 app = Flask('__name__')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite3'
+app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.sqlite3'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-salt = "saltedhash"
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(), unique=True)
+    password = db.Column(db.String())
+    todos = db.relationship('Todo', backref='todos')
+
+
+    def __repr__(self):
+        return f"User: {self.username}"
 
 class Todo(db.Model):
     __tablename__ = 'todo'
@@ -18,20 +28,33 @@ class Todo(db.Model):
     description = db.Column(db.String())
     important = db.Column(db.String())
     done = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(), unique=True)
-    password = db.Column(db.String())
+    def __repr__(self):
+        return f"Todo: {self.todo}"
 
 with app.app_context():
     db.create_all()
 
+def hashed(data):
+    salt = "saltedhash"
+    data = str(data) + salt
+    return hashlib.md5(data.encode()).hexdigest()
+
+def is_authenticated():
+    user = session.get("username")
+    if not user:
+        return False
+    else:
+        return True
+
 @app.route('/')
 def main():
-    todos = Todo.query.all()
-    return render_template('index.html', todos=todos)
+    if is_authenticated():
+        todos = Todo.query.all()
+        return render_template('index.html', todos=todos)
+    else:
+        return jsonify({"Error": "User not found"})
 
 @app.route('/all_todos', methods=['POST', 'GET'])
 def all_todos():
@@ -54,7 +77,10 @@ def all_todos():
 def todo_id(id):
     if request.method == "GET":
         get_todo = Todo.query.get(id)
-        return jsonify({"todo": get_todo.todo, "description": get_todo.description})
+        if not get_todo:
+            return jsonify({"Error": "ID not found"})
+        else:
+            return jsonify({"todo": get_todo.todo, "description": get_todo.description})
     elif request.method == "DELETE":
         get_todo = Todo.query.get(id)
         db.session.delete(get_todo)
@@ -65,26 +91,23 @@ def todo_id(id):
 def register():
     if request.method == "POST":
         user = request.get_json()["username"]
-        salted_password = request.get_json()["password"] + salt
-        hashed = hashlib.md5(salted_password.encode())
-        db_todo = User(username=user, password=hashed.hexdigest())
+        user_password = request.get_json()["password"]
+        db_todo = User(username=user, password=hashed(user_password))
         db.session.add(db_todo)
         db.session.commit()
-        return jsonify({"Added": f"{user}: {hashed.hexdigest()}"})
+        return jsonify({"Added": f"{user}: {hashed(user_password)}"})
 
 @app.route('/login', methods=['GET'])
 def login():
     if request.method == "GET":
         user = request.get_json()["username"]
-        print(user)
-        salted_password = request.get_json()["password"] + salt
-        hashed = hashlib.md5(salted_password.encode())
+        user_password = request.get_json()["password"]
         db_user = User.query.filter_by(username=user).first()
         if db_user:
-            if hashed.hexdigest() == db_user.password:
+            if hashed(user_password) == db_user.password:
+                session["username"] = hashed(user)
                 return jsonify({"User": f"{db_user.username} is logged in"})
             else:
                 return jsonify({"Error": "Password is incorrect"})
         else:
             return jsonify({"Error": "User not found"})
-
